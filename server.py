e13e2c2d5c4111f842fec39749bc8a1ff5a774bc
jdavids0 <jbrdavidson@gmail.com>
2022-04-07 12:48:26 -0600

    edit

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
-from typing import Counter
 from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 app.secret_key = 'Rand0m$!'
 
-@app.route('/') 
+@app.route('/', methods=['GET', 'POST']) 
 def index():
-    if 'count' in session:
-        session ['count'] += 1
+    if request.form['increment'] == '':
+        if 'count' in session:
+            session ['count'] += 1
+        else:
+            session ['count'] = 0
+        return render_template('index.html')
     else:
-        session ['count'] = 0
-    return render_template('index.html')
+        if 'increment' in session:
+            session ['increment'] += request.form['increment']
+        else: 
+            session ['increment'] = 1
+        return render_template ('index.html')
+    
 
 @app.route('/count2')
 def count2():
@@ -22,14 +29,7 @@ def count2():
 @app.route('/destroy_session')
 def destroySession():
     session.clear()
-    return redirect('/')
-    
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     print(request.form)
-#     session ['username'] = request.form['name']
-#     session ['useremail'] = request.form['email']
-#     return redirect("/show")	
+    return redirect('/')	
 
 if __name__=="__main__":
     app.run(debug=True, port=5001) 
